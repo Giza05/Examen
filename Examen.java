b4ee28c45544e90221b67cb5b7ae81894677cc4b
Giza05 <125763286+Giza05@users.noreply.github.com>
2023-03-25 10:06:11 -0600

    Add files via upload

---
+++ Examen.java
@@ -0,0 +1,24 @@
+
+import java.util.ArrayList;
+import java.util.List;
+public class Examen {
+
+        public static void main(String[] args) {
+            List alumnos = new ArrayList();
+            alumnos.add("Juan");
+            alumnos.add("Leonardo");
+            alumnos.add("Patricia");
+            alumnos.add("Diego");
+            alumnos.add("Elizabeth");
+            alumnos.add("Maria");
+            alumnos.add("Rosa");
+            alumnos.add("Luis");
+            alumnos.add("Mariel");
+            alumnos.add("Enrique");
+
+
+            System.out.println(alumnos);
+
+        }
+    }
+
